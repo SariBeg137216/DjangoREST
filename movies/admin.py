d918bdc3e7cc7a8bbbfc71de7d5fb8f040287b0e
SariBeg137216 <farmaniansaribeg@gmail.com>
2021-05-05 13:50:20 +0430

    admin panel customizing

--- movies/admin.py
+++ movies/admin.py
@@ -3,4 +3,21 @@ from .models import Moviedata
 
 # Register your models here.
 
-admin.site.register(Moviedata)
+admin.sites.AdminSite.site_header = "Movies"
+admin.sites.AdminSite.site_title = "MovieAdminPanel"
+admin.sites.AdminSite.index_title = "MoviesIndexList"
+
+
+class MovieAdmin(admin.ModelAdmin):
+
+    def change_typ_to_default(self, request, queryset):
+        queryset.update(typ='default')
+
+    change_typ_to_default.short_description = "typ-change"
+    list_display = ('name', 'rating', 'duration', 'typ')
+    list_editable = ('duration', 'typ')
+    fields = ('name',)
+    actions = ('change_typ_to_default',)
+
+
+admin.site.register(Moviedata, MovieAdmin)
